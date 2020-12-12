60cd69c42e3dd514ff154cbbcae52a275220af15
Highlander1952 <68139321+Highlander-maker@users.noreply.github.com>
2020-12-12 22:11:39 +0000

    Add files via upload

---
+++ Frontend/abi.js
@@ -0,0 +1,32 @@
+var abi = [
+ {
+   "constant": true,
+   "inputs": [],
+   "name": "getMessage",
+   "outputs": [
+     {
+       "internalType": "string",
+       "name": "",
+       "type": "string"
+     }
+   ],
+   "payable": false,
+   "stateMutability": "view",
+   "type": "function"
+ },
+ {
+   "constant": false,
+   "inputs": [
+     {
+       "internalType": "string",
+       "name": "newMessage",
+       "type": "string"
+     }
+   ],
+   "name": "setMessage",
+   "outputs": [],
+   "payable": true,
+   "stateMutability": "payable",
+   "type": "function"
+ }
+]
---
+++ migrations/2_Coinflip_deploy.js
@@ -0,0 +1,10 @@
+const Coinflip = artifacts.require("Coinflip");
+
+module.exports = function(deployer, network, accounts){
+  deployer.deploy(Coinflip);
+  let contract = await Coinflip.deployed();
+await contract.setMessage('new message',{from:accounts[0], value:100000000});
+let msg = await contract.getMessage();
+console.log(msg)
+
+};
