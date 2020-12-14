d42395cb7e4c75dbac0d5e5ca6f9de66dae37b3a
Highlander1952 <68139321+Highlander-maker@users.noreply.github.com>
2020-12-14 22:08:45 +0000

    Update main.js

--- Frontend/main.js
+++ Frontend/main.js
@@ -2,28 +2,47 @@ var web3 = new Web3(Web3.givenProvider);
 var contractInstance;
 
 $(document).ready(function() {
+  //THIS BRINGS UP METAMASK
     window.ethereum.enable().then(function(accounts){
-      contractInstance = new web3.eth.Contract(window.abi, "0xc22f6f52Ad85613b4a27eD86Dd414Be9952dF3e6", {from: accounts[0]});
-      console.log(contractInstance);
+        contractInstance = new web3.eth.Contract(abi, "0xcbB04FE63A98C9adBf7Ca158143eB7Db4559881D", {from: accounts[0]});
+        console.log(contractInstance);
     });
-    $("#add_data_button").click(inputData)
+    $("#bet_button_heads").click(inputData)
+    $("#bet_button_tails").click(inputData)
+    $("#get_data_button").click(fetchAndDisplay)
+});
 
-
-
-  });
-
-  function inputData(){
-  var bet amount = $("#Bet Amount_input").val();
+function inputData(){
 
   var config = {
     value: web3.utils.toWei("1", "ether")
-  })
+  }
 
+  contractInstance.methods.play(1).send(config)
+  .on("transactionHash", function(hash){
+      console.log(hash);
+  })
   .on("confirmation", function(confirmationNr){
-    console.log(confirmationNr);
+      console.log(confirnationNr);
   })
   .on("receipt", function(receipt){
-    console.log(receipt);
-  });
+    console.log(receipt.events.bet.returnValues[0]);
 
-};
+    if (receipt.events.bet.returnValues[0] == true)  {
+      $("#message_output").text("You won 1 Ether!");
+      party.screen();
+    } else {
+      $("#message_output").text("You lost 1 Ether!");
+    }
+  })
+}
+
+function fetchAndDisplay(){
+  contractInstance.methods.getPerson().call().then(function(res){
+    $("#name_output").text(res.name);
+    $("#age_output").text(res.age);
+    $("#height_output").text(res.height);
+
+  })
+
+}
